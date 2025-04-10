dff994c3c4d8cd7e586f0d6d83659ce74eb15a8c
Okafor Ifeanyi <ifeanyi.okafor.bio@gmail.com>
2025-04-10 18:07:08 +0100

    feat(app): urgent

---
+++ src/components/InfoBox.tsx
@@ -0,0 +1,13 @@
+import { FederalGovt } from "./Federal"
+// import { LocalGovt } from "./Local"
+// import { StateGovt } from "./state"
+
+export const InfoBox = () => {
+    return (
+        <div>
+            <FederalGovt />
+            {/* <StateGovt />
+            <LocalGovt /> */}
+        </div>
+    )
+}
---
+++ src/components/State.tsx
@@ -0,0 +1,5 @@
+export const StateGovt = () => {
+  return (
+    <div>state</div>
+  )
+}
